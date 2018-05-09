6d4fb87e9e6ee70053d91ea0ec1463e53e055930
koplar <koplar@petra.ac.id>
2018-05-09 14:44:29 +0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+/** Tambahan */
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+/** --- */
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers : [InAppBrowser]
 })
 export class HomePage {
+  scannedQrCode = null;
+  qrData = null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public barcodeScanner : BarcodeScanner,
+    public inAppBrowser : InAppBrowser) {
 
+  }
+
+  // menscan qrdata
+  scanQRCode() : void{
+    this.barcodeScanner.scan()
+      .then(barcodeData => {
+        this.scannedQrCode = barcodeData.text;
+      }, (error) => {
+        console.log('Scan Error : ', error);
+      });
+  }
+
+  linkUrl() : void {
+    const openBrowser = this.inAppBrowser.create(
+      this.scannedQrCode,
+      '_self', // atau _self
+      'location=yes'
+    );
+    openBrowser.show();
+  }
+
+  linkUrlnew() : void {
+    const openBrowser = this.inAppBrowser.create(
+      'http://inixindosurabaya.id',
+      '_self', // atau _self
+      'location=yes'
+    );
+    openBrowser.show();
   }
 
 }
